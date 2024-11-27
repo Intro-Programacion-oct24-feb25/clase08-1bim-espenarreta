f92be1d4ffa1b6b88ea2fa5fc6d2c2fb59d77f36
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-11-27 11:45:19 -0500

    Cambios

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -7,6 +7,10 @@
  sean ingresados por teclado por el usuario.
  */
 package ejemplos02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -14,6 +18,8 @@ package ejemplos02;
 public class Ejemplo05 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         // 2. numero, i[0, n]
         int numero;
         // 3. potencia, i[0, n]
@@ -21,9 +27,11 @@ public class Ejemplo05 {
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
+        System.out.println("Ingrese el número");
+        numero = entrada.nextInt();
         // 8. potencia <-- 3
-        potencia = 3;
+        System.out.println("Ingrese la potencia");
+        potencia = entrada.nextInt();
         // 9. resultado <-- 1
         resultado = 1;
 
@@ -34,6 +42,6 @@ public class Ejemplo05 {
             contador = contador + 1;
         } while (contador <= potencia);
 
-        System.out.printf("%d\n", resultado);
+        System.out.printf("Resultado: %d\n", resultado);
     }
 }
